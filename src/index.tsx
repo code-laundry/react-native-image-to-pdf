62f150419ece53abd25d07af5d46954fa929b4ba
Nicola Reggiani <nicola.reggiani@codelaundry.io>
2023-02-17 16:54:57 +0100

    Changed project structure and added TS

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-image-to-pdf' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const ImageToPdf = NativeModules.ImageToPdf
+  ? NativeModules.ImageToPdf
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function createPDFbyImages(options: any): Promise<any> {
+  return ImageToPdf.createPDFbyImages(options);
+}
